9be0371db9a21e2c8c5f252a45c22783d05f04dc
chongin119 <macaotom@gmail.com>
2021-12-03 11:10:48 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,97 @@
+from flask import Flask,request,flash,session
+from flask.helpers import url_for
+from flask.templating import render_template
+from flask_bootstrap import Bootstrap
+from flask import g
+from datetime import timedelta
+
+from werkzeug.utils import redirect
+from dbfunc import *
+
+
+app = Flask(__name__)
+databasePATH = "testing.db"
+app.config['SECRET_KEY'] = "my_secret_key"
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=1)
+bootstrap = Bootstrap(app)
+
+
+@app.route('/',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        db = connect_db(databasePATH)
+        judge = match_user_pwd(db,username,password)
+        disconnect_db(db)
+
+        if judge == True:
+
+            if session.get(username) is None:
+                session[username] = username
+                session.permanent = True
+
+            db = connect_db(databasePATH)
+            domain = int(get_domain(db,username))
+            disconnect_db(db)
+            
+            if domain == 0:
+                return redirect(url_for('doctor',username = username))
+            elif domain == 1:
+                return redirect(url_for('doctor',username = username))
+            elif domain == 2:
+                return redirect(url_for('patient',username = username))
+        else:
+            flash('login failed!')
+            return redirect(url_for('login'))
+    return render_template('login.html')
+
+@app.route('/logout')
+@app.route('/logout?<string:username>')
+def logout(username=None):
+    if session.get('username') == username:
+        session.pop('username')
+    return redirect(url_for('login'))
+
+
+@app.route('/forget_password')
+def forget_password():
+    return render_template('forget_password.html')
+
+@app.route('/register' , methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        repeat_password = request.form['repeat_password']
+
+        if password != repeat_password:
+            flash('password is not equal to confirm_password!')
+            return redirect(url_for('register'))
+
+        db = connect_db(databasePATH)
+        judge = insert_user_pwd(db,username,password)
+        disconnect_db(db)
+
+        if judge == True:
+            flash('Register Success!')
+            return redirect(url_for('login'))
+        else:
+            return redirect(url_for('register'))
+    return render_template('register.html')
+
+@app.route('/patient/?<string:username>',methods=['GET','POST'])
+def patient(username):
+    if session.get(username) is not None:
+        return render_template('patient.html',name = username)
+    return redirect(url_for('login'))
+
+@app.route('/doctor/?<string:username>',methods=['GET','POST'])
+def doctor(username):
+    if session.get(username) is not None:
+        return render_template('doctor.html',name = username)
+    return redirect(url_for('login'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ dbfunc.py
@@ -0,0 +1,51 @@
+import sqlite3
+from sqlite3.dbapi2 import connect
+
+DB_path = 'testing.db'
+
+def connect_db(DB_path):
+    db  = sqlite3.connect(DB_path)
+    return db
+
+def disconnect_db(db):
+    db.close()
+
+def insert_user_pwd(db,user,pwd):
+    cur = db.cursor()
+    last_id_cur = cur.execute('''select max(id) 
+                            from login_inf      
+                        ''')
+    for i in last_id_cur:
+        last_id = i[0]
+    last_id = last_id + 1
+    
+    try :
+        cur.execute("insert into login_inf Values ('%d','%s','%s',0)" %(last_id, user, pwd))
+        db.commit()
+        return True
+    except sqlite3.Error:
+        return False
+
+def match_user_pwd(db,user,pwd):
+    cur = db.cursor()
+    tt = cur.execute("select username,password \
+                        from login_inf ")
+    for i in tt:
+        if i[0] == user and i[1] == pwd:
+            return True
+    return False
+
+def get_domain(db,user):
+    cur = db.cursor()
+    tt = cur.execute("select domain \
+                      from login_inf \
+                      where username = '%s'" %user)
+    for i in tt:
+        return i[0]
+
+if __name__ == '__main__':
+    db = connect_db(DB_path)
+    #abc = insert_user_pwd(db,'ro3ot','root')
+    abc = get_domain(db,'root')
+    print(abc)
+    disconnect_db(db)
