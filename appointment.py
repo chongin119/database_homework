dd4003973282aeb4f76c2457670116795bfa9d35
coolzifan123 <2814299761@qq.com>
2021-12-24 16:52:46 +0800

    增加医生预约上限

--- appointment.py
+++ appointment.py
@@ -8,7 +8,7 @@ from sliderbaritem import patientItems
 bp = Blueprint('appointment', __name__)
 
 db = connect_db(databasePATH)
-
+APP_NUM = 20
 def get_id(db,user):
     cur = db.cursor()
     tt = cur.execute("select patient_id \
@@ -19,29 +19,44 @@ def get_id(db,user):
 
 @bp.route('/patient/?<string:username>/patient_appointments',methods=['GET','POST'])
 def patient_appointments(username):
+    patient_id = get_id(db,username)
     appointments = db.execute(
-        '''SELECT date, department_name, name
-        FROM appointment a LEFT JOIN employees e ON e_id = doc_id 
-        LEFT JOIN department d ON d.department_id = a.department_id 
-         ORDER BY date DESC'''
+        "SELECT date, department_name, name\
+        FROM appointment a LEFT JOIN employees e ON e_id = doc_id \
+        LEFT JOIN department d ON d.department_id = a.department_id \
+        WHERE patient_id=? ORDER BY date DESC", (patient_id,)
     ).fetchall()
     return render_template('patient_appointments.html', name = username,sidebarItems=patientItems,appointments=appointments,hav = len(appointments))
 
 @bp.route('/patient/?<string:username>/add_appointment',methods=['GET','POST'])
 def patient_add_appointment(username):
+    patient_id = get_id(db,username)
+
     if request.method == 'POST':
-        """api to add the patient in the database"""
+        """api to add the appointment in the database"""
         app_date = request.form['date']
         patient_id = get_id(db,username)
         department_id = request.form['de_id']
         doc_id = request.form['doc_id']
         survey = None
+        error = None
+        number = db.execute("SELECT count(*) FROM appointment a \
+                                INNER JOIN employees e ON e_id = doc_id \
+                                INNER JOIN patient p ON a.patient_id = p.patient_id \
+                                WHERE e_id=? ORDER BY date DESC", (doc_id,)).fetchall()
+        number = number[0][0]
+        if number >= APP_NUM:
+            error = "The doctor's appointments are full that day"
+        if error is not None:
+            flash(error)
+            redirect(url_for('appointment.patient_appointments'))
         app_id = db.execute('''INSERT INTO appointment(date,patient_id,department_id,doc_id,epidemic_survey)
                     VALUES(?,?,?,?,?)''', (app_date, patient_id, department_id, doc_id, survey)).lastrowid
         db.commit()
-        return redirect(url_for('appointment.appointments'))
+        return redirect(url_for('appointment.patient_appointments'))
 
     return render_template('patient_add_appointment.html', name = username,sidebarItems=patientItems)
+
 
 
 
