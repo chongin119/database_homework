08d586d231d090a5ac6dfb89aa276f1270e038e2
coolzifan123 <2814299761@qq.com>
2021-12-29 19:57:17 +0800

    医生历史记录和修改信息后端

--- doctor.py
+++ doctor.py
@@ -4,7 +4,7 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from dbfunc import connect_db,match_user_pwd,disconnect_db,get_domain,insert_user_pwd
-from dbfunc import databasePATH
+from dbfunc import databasePATH,check_repeat
 from sliderbaritem import doctorItems
 bp = Blueprint('doctor', __name__)
 
@@ -26,13 +26,75 @@ def doctor(username):
     return redirect(url_for('auth.login'))
 
 
-@bp.route('/doctor/?<string:username>/history',methods=['GET', 'POST'])
+@bp.route('/doctor/?<string:username>/history',methods=['GET','POST'])
 def history(username):
     doc_id = get_id(db, username)
-    prescription = db.execute("SELECT date , p.name, p.phone, med_id, med_quantity, med_name,  FROM prescription pre \
-                                INNER JOIN patient p ON p.patient_id = pre.patient_id \
-                                WHERE e_id=? ORDER BY date DESC", (doc_id,)).fetchall()
-    return render_template('doctor_pre.html', prescription=prescription)
+
+    # 格式为(病人姓名，日期，药品名字，药品用量,体温，主诉，现病史，既往史，过敏史，发病时间，治疗情况，评估诊断)
+    prescriptions_records = db.execute('''SELECT p.name,p.date,med_name,med_quantity,temperature,chief_complaint,
+    present_illness_history,past_history, allergic_history， onset_date,current_treatment, diagnostic_assessment
+    FROM prescription p INNER JOIN employees e ON e.e_id = p.doc_id
+    INNER JOIN medicine m ON m.med_id = p.med_id 
+    LEFT JOIN medical_record r ON p.app_id = r.app_id
+    WHERE p.doc_id=? AND p.date<=? ORDER BY p.date DESC''', (doc_id, datetime.date.today())).fetchall()
+
+    return render_template('xxx.html')
+
+@bp.route('/doctor/?<string:username>/change_inf/' , methods=['GET','POST'])
+def change_inf(username):
+    doc_id = get_id(db,username)
+    if request.method == "POST":
+        user = request.form['username']
+        phone = request.form['phone']
+        email = request.form['email']
+        pwd = request.form['pwd']
+        repwd = request.form['repwd']
+        graduate_school = request.form['school']
+        degree = request.form['degree']
+        technical_title = request.form['title']
+        specialty = request.form['specialty']
+        if pwd != repwd:
+            flash('password is not equal to confirm_password!')
+            return redirect(url_for('doctor.doctor'))
+        if check_repeat(db, user):
+            flash('The username already exists')
+            return redirect(url_for('doctor.doctor'))
+        if pwd != "NULL":
+            db.execute('''UPDATE login_inf 
+            SET username = ?, password=?
+            WHERE username = ?''', (user, pwd, username))
+            db.execute(
+                "UPDATE employees SET phone=?,email=?,username=?,password=?,graduate_school=?\
+                , degree=?, technical_title=?,specialty=?\
+                 WHERE e_id=?",
+                (phone, email, user, pwd, graduate_school, degree, technical_title, specialty))
+            db.commit()
+        else:
+            db.execute('''UPDATE login_inf 
+                        SET username = ?
+                        WHERE username = ?''', (user, username))
+            db.execute(
+                "UPDATE employees SET phone=?,email=?,username=?,graduate_school=?\
+                , degree=?, technical_title=?,specialty=?\
+                 WHERE e_id=?",
+                (phone, email, user, graduate_school, degree, technical_title, specialty))
+            db.commit()
+        flash('Successfully modified information')
+        return redirect(url_for('doctor.doctor', username=user))
+
+
+    allinf = db.execute('''
+                                SELECT e_id,name,passport,gender,phone,email,username,graduate_school
+                                FROM employees
+                                WHERE username =?   
+                            ''',(username,)).fetchall()
+
+    #print(allinf[0])
+    i, j, k, l, m, n, o,p = allinf[0][0], allinf[0][1], allinf[0][2], allinf[0][3], allinf[0][4], allinf[0][5],allinf[0][6],allinf[0][7]
+
+    infdic = [j, k, l, m, n, o,p]
+    #print(infdic)
+    return render_template('doctor_change_inf.html', name = username,sidebarItems = doctorItems,allinf = infdic)
 
 @bp.route('/doctor/?<string:username>/diagnosis',methods=['GET', 'POST'])
 def diagnosis(username):
@@ -40,7 +102,7 @@ def diagnosis(username):
     appointments = db.execute("SELECT date , p.name, p.phone FROM appointment a \
                                     INNER JOIN employees e ON e_id = doc_id \
                                     INNER JOIN patient p ON a.patient_id = p.patient_id \
-                                    WHERE e_id=?,date = ? ORDER BY date DESC", (doc_id,datetime.date.today())).fetchall()
+                                    WHERE e_id=? AND date = ? ORDER BY date DESC", (doc_id,datetime.date.today())).fetchall()
     return render_template('diagnosis.html',appointments=appointments)
 
 @bp.route('/doctor/?<string:username>/diagnosis/<id>',methods=['GET', 'POST'])
