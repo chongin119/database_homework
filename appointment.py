2250e0b59d7a291b33463925bf9158e4b4ada599
chongin119 <macaotom@gmail.com>
2022-01-01 21:01:33 +0800

    完成所有 累了

--- appointment.py
+++ appointment.py
@@ -93,7 +93,7 @@ def patient_add_appointment(username):
         app_date = request.form['date']
         department_id = request.form['de_id']
         doc_id = request.form['doc_id']
-        print(doc_id)
+        #print(doc_id)
         survey = None
         error = None
 
@@ -194,13 +194,20 @@ def admin_appointments(username):
                                 INNER JOIN employees e ON e_id = doc_id \
                                 INNER JOIN patient p ON a.patient_id = p.patient_id", ).fetchall()
     log_write(user=username, action='visit', dist='appointment')
-    return render_template('admin_appointments.html',name = username,sidebarItems=adminItems,appointments=appointments,hav = len(appointments))
+
+    appdic = {}
+    for i in appointments:
+        appdic[i[0]] = [i[1],i[2],i[3],i[4],i[5],i[6],i[7],i[8],i[9]]
+    return render_template('admin_appointments.html',name = username,sidebarItems=adminItems,patients=appdic,hav = len(appointments))
 
 
 @bp.route('/admin/?<string:username>/add_appointment',methods=['GET', 'POST'])
 def admin_add_appointment(username):
     # 给出病人列表
     patient_list = db.execute('''SELECT patient_id,name FROM patient''')
+    patdic = {}
+    for i in patient_list:
+        patdic[i[0]] = i[1]
     # 给出部门列表
     all_departments = db.execute('''
                                         SELECT department_name,department_id
@@ -213,6 +220,36 @@ def admin_add_appointment(username):
                                 INNER JOIN employees e ON e_id = doc_id
                                 INNER JOIN department m ON a.department_id = m.department_id
                             ''').fetchall()
+    departments = db.execute("SELECT * FROM department").fetchall()
+    dicdep = {}
+    for cnt in range(len(departments)):
+        i, j, k = departments[cnt][0], departments[cnt][1], departments[cnt][2]
+        dicdep[i] = [j, k]
+    # print(dicdep)
+
+    doctorfromdepartments = db.execute('''
+                                                    SELECT doc_id,d.department_id
+                                                    FROM doctor d INNER JOIN department e ON d.department_id == e.department_id 
+                                                ''').fetchall()
+    dfddic = {}
+    for cnt in range(len(doctorfromdepartments)):
+        i, j = doctorfromdepartments[cnt][0], doctorfromdepartments[cnt][1]
+        if dfddic.get(j) == None:
+            dfddic[j] = [i]
+        else:
+            dfddic[j].append(i)
+
+    alldoc = db.execute('''
+                                    SELECT e_id,name,phone,email,graduate_school,degree,technical_title,specialty
+                                    FROM employees
+                                ''').fetchall()
+
+    docdic = {}
+    for cnt in range(len(alldoc)):
+        i, j, k, l, m, n, o, p = alldoc[cnt][0], alldoc[cnt][1], alldoc[cnt][2], alldoc[cnt][3], alldoc[cnt][4], \
+                                 alldoc[cnt][5], alldoc[cnt][6], alldoc[cnt][7]
+        if docdic.get(i) == None:
+            docdic[i] = [j, k, l, m, n, o, p]
 
     if request.method == 'POST':
         """api to add the appointment in the database"""
@@ -222,9 +259,9 @@ def admin_add_appointment(username):
         department_id = request.form['de_id']
         doc_id = request.form['doc_id']
         temperature = request.form['temperature']
-        province = '北京'  # 未完成 先设为这个
-        city = '市辖区'  # 未完成 先设为这个
-        district = '海淀区'  # 未完成 先设为这个
+        province = request.form['province']
+        city = request.form['city']
+        district = request.form['district']
         symptom = request.form['rm']  # 返回为有症状或无症状
         risk = request.form['r14']  # 返回为曾到或未曾到
 
@@ -235,9 +272,9 @@ def admin_add_appointment(username):
                            (app_date, patient_id, department_id, doc_id, temperature,address,symptom,risk)).lastrowid
         db.commit()
         log_write(user=username, action='add', dist='appointment')
-        return redirect(url_for('appointment.admin_appointments',username=username))
+        return render_template('loading.html')
 
-    return render_template('patient_add_appointment.html', name = username,sidebarItems = adminItems,patient_list=patient_list,all_doctors=all_doctors,all_departments=all_departments)
+    return render_template('admin_appointment_add_working.html', patdic = patdic,addressdic = addressdic,name = username,sidebarItems = adminItems,patient_list=patient_list,all_doctors=all_doctors,alldepartments = dicdep,dfd = dfddic,docdic = docdic)
 
 
 @bp.route('/admin/?<string:username>/update_appointment/<id>', methods=['GET', 'POST'])
@@ -255,6 +292,72 @@ def admin_update_appointment(username,id):
                                 INNER JOIN employees e ON e_id = doc_id
                                 INNER JOIN department m ON a.department_id = m.department_id
                             ''').fetchall()
+    appointments = db.execute('''SELECT app_id,date , p.name, a.patient_id, e.name,e_id, a.temperature,symptom,address,risk 
+                                    FROM appointment a 
+                                    INNER JOIN employees e ON e_id = doc_id 
+                                    INNER JOIN patient p ON a.patient_id = p.patient_id 
+                                    WHERE app_id = ?''',(app_id,)).fetchall()
+
+    rm = "'"+str(appointments[0][7])+"'"
+    r14 = "'"+str(appointments[0][9])+"'"
+
+    defaultalldoctor = db.execute('''
+                                        SELECT d.doc_id,e.name
+                                        FROM appointment a INNER JOIN doctor d
+                                        INNER JOIN employees e ON e.e_id = d.doc_id
+                                        WHERE a.app_id = ? and a.department_id = d.department_id
+                                    ''',(app_id,)).fetchall()
+
+    defdic = {}
+    for i in defaultalldoctor:
+        defdic[i[0]] = i[1]
+
+    ddocid = appointments[0][5]
+    ddocname = appointments[0][4]
+    ddepid  = db.execute('''
+                            SELECT department_id
+                            FROM doctor
+                            WHERE doc_id =?
+                        ''',(ddocid,)).fetchone()
+    ddepid = ddepid[0]
+    ddepname = db.execute('''
+                            SELECT department_name
+                            FROM department
+                            WHERE department_id =?
+                            ''',(ddepid,)).fetchone()
+    ddepname = ddepname[0]
+
+    departments = db.execute("SELECT * FROM department").fetchall()
+    dicdep = {}
+    for cnt in range(len(departments)):
+        i, j, k = departments[cnt][0], departments[cnt][1], departments[cnt][2]
+        dicdep[i] = [j, k]
+    # print(dicdep)
+
+    doctorfromdepartments = db.execute('''
+                                                SELECT doc_id,d.department_id
+                                                FROM doctor d INNER JOIN department e ON d.department_id == e.department_id 
+                                            ''').fetchall()
+    dfddic = {}
+    for cnt in range(len(doctorfromdepartments)):
+        i, j = doctorfromdepartments[cnt][0], doctorfromdepartments[cnt][1]
+        if dfddic.get(j) == None:
+            dfddic[j] = [i]
+        else:
+            dfddic[j].append(i)
+
+    alldoc = db.execute('''
+                                SELECT e_id,name,phone,email,graduate_school,degree,technical_title,specialty
+                                FROM employees
+                            ''').fetchall()
+
+    docdic = {}
+    for cnt in range(len(alldoc)):
+        i, j, k, l, m, n, o, p = alldoc[cnt][0], alldoc[cnt][1], alldoc[cnt][2], alldoc[cnt][3], alldoc[cnt][4], \
+                                 alldoc[cnt][5], alldoc[cnt][6], alldoc[cnt][7]
+        if docdic.get(i) == None:
+            docdic[i] = [j, k, l, m, n, o, p]
+
     log_write(user=username, action='edit', dist='appointment')
     if request.method == 'POST':
         """api to add the appointment in the database"""
@@ -263,9 +366,9 @@ def admin_update_appointment(username,id):
         department_id = request.form['de_id']
         doc_id = request.form['doc_id']
         temperature = request.form['temperature']
-        province = '北京'  # 未完成 先设为这个
-        city = '市辖区'  # 未完成 先设为这个
-        district = '海淀区'  # 未完成 先设为这个
+        province = request.form['province']
+        city = request.form['city']
+        district = request.form['district']
         symptom = request.form['rm']  # 返回为有症状或无症状
         risk = request.form['r14']  # 返回为曾到或未曾到
 
@@ -275,10 +378,10 @@ def admin_update_appointment(username,id):
                    , (app_date, department_id, doc_id, temperature, address, symptom, risk,app_id))
         db.commit()
 
-        return redirect(url_for('appointment.admin_appointments', username=username))
+        return render_template('loading.html')
 
-    return render_template('patient_add_appointment.html', name=username, sidebarItems=adminItems
-                           , all_doctors=all_doctors, all_departments=all_departments)
+    return render_template('admin_appointment_update_working.html',defdic = defdic, rm = rm,r14 = r14,addressdic = addressdic,ddepname = ddepname,ddocname = ddocname,ddocid = ddocid,ddepid = ddepid,appid = app_id,name=username, sidebarItems=adminItems
+                           , alldepartments = dicdep,dfd = dfddic,docdic = docdic, appo = appointments)
 
 @bp.route('/admin/?<string:username>/delete_appointment/<id>', methods=['GET', 'POST'])
 def admin_delete_appointment(username):
@@ -292,7 +395,7 @@ def admin_delete_appointment(username):
     log_write(user=username, action='delete', dist='prescription')
     db.commit()
 
-    return redirect(url_for('appointment.admin_appointments', username=username))
+    return render_template('loading.html')
 
 # ------------------------------------------------------
 
